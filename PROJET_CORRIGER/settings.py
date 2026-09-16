9615e0552e7d4fed68cb3d14c5aa40cfa94bbf50
dieumercibaderha <129082152+dieumercibaderha@users.noreply.github.com>
2026-09-16 16:13:43 +0200

    Update settings.py

--- PROJET_CORRIGER/settings.py
+++ PROJET_CORRIGER/settings.py
@@ -157,5 +157,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'projettutore401@gmail.com'
-EMAIL_HOST_PASSWORD = 'xgsubmznhopgdtnk'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
